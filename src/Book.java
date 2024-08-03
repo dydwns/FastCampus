bf8d618cf3f741ccc46df4cdb3f40b8532ba795a
dydwns <kaka2438@hanmail.net>
2024-08-03 22:16:08 +0900

    ✨ 새로운 자료형 만들기

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+
+    public String title;
+    public int price;
+    public String author;
+    public String company;
+    public int page;
+    public String isbn;
+
+
+}
---
+++ src/BookTest.java
@@ -0,0 +1,19 @@
+public class BookTest {
+    public static void main(String[] args) {
+
+        int a;
+        a = 10;
+        System.out.println("a = " + a);
+
+        String title = "자바";
+        int price = 30000;
+        String company = "패스트캠퍼스";
+        String autnor = "이용준";
+        int page = 700;
+        String isbn = "1234567890";
+        System.out.println(title + "\t" + price + "\t" + company + "\t" + autnor + "\t" + page + "\t" + isbn);
+
+        Book b;
+
+    }
+}
