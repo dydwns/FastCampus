ee61c6c51fc32e7b3ff5d91026a76d9edf3c1e7a
dydwns <kaka2438@hanmail.net>
2024-08-04 20:14:19 +0900

    ✨ 배열 연습

---
+++ src/ArrayBasicTest.java
@@ -0,0 +1,13 @@
+public class ArrayBasicTest {
+    public static void main(String[] args) {
+
+        int a, b, c;
+        a = 10;
+        b = 10;
+        c = 10;
+        int sum = a + b + c;
+        System.out.println("sum = " + sum);
+
+
+    }
+}
