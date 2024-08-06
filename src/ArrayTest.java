a240f706c49fa07b4131a46e234082ba3f6b435b
dydwns <kaka2438@hanmail.net>
2024-08-06 21:13:42 +0900

    ✨ 배열의 선언과 초기화

---
+++ src/ArrayTest.java
@@ -0,0 +1,28 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+
+        float[] f = new float[5];
+        f[0] = 10.5f;
+        f[1] = 15.5f;
+        f[2] = 50.5f;
+        f[3] = 20.5f;
+        f[4] = 88.5f;
+        for (int i = 0; i < f.length; i++) {
+            System.out.println(f[i]);
+        }
+
+        int[] a = {10, 20, 30, 40, 50};
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        int sum = a[0] + a[3];
+        System.out.println("sum = " + sum);
+        System.out.println(a.length);
+
+        int[] b = new int[3];
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+
+    }
+}
