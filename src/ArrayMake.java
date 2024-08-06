f43b0c9107856f23335a492c56b50b2d93ad1d7d
dydwns <kaka2438@hanmail.net>
2024-08-06 20:19:20 +0900

    ✨ 배열 만들기

---
+++ src/ArrayMake.java
@@ -0,0 +1,12 @@
+public class ArrayMake {
+    public static void main(String[] args) {
+        int [] a;
+        a = new int[5];
+
+        float[] f = new float[5];
+
+        Book b1, b2, b3;
+        Book[] b = new Book[3];
+
+    }
+}
