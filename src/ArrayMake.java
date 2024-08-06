10afacccf73c426f3218f0e4f33d6ef5cd9d9476
dydwns <kaka2438@hanmail.net>
2024-08-06 20:36:50 +0900

    ✨ 배열 반복문 적용

--- src/ArrayMake.java
+++ src/ArrayMake.java
@@ -1,7 +1,12 @@
 public class ArrayMake {
     public static void main(String[] args) {
-        int [] a;
-        a = new int[5];
+        int[] a = new int[5];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = 10;
+            System.out.println(a[i]);
+        }
+
 
         float[] f = new float[5];
 
